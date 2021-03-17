5d855f44970327a3ed9cfb9672fd057f22338fde
rr <ryohamadapride@gmail.com>
2021-03-18 00:11:38 +0900

    Initial Commit

---
+++ TimerProto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TimerProto
+//
+//  Created by 浜田諒 on 2021/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimerProto/TimerProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerProtoApp.swift
+//  TimerProto
+//
+//  Created by 浜田諒 on 2021/03/18.
+//
+
+import SwiftUI
+
+@main
+struct TimerProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
