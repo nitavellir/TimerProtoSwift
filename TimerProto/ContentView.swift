59a0b9a33b1a72a7d28e8e0e33cdde2de1aeb55c
rr <ryohamadapride@gmail.com>
2021-03-28 12:12:05 +0900

    timer proto 1

--- TimerProto/ContentView.swift
+++ TimerProto/ContentView.swift
@@ -7,10 +7,78 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct TimerView: View {
+    @Binding var timerScreenShow:Bool
+    @Binding var timeVal:Int
+    
+    @State var timer:Timer?
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group{
+            if timeVal > -1 {
+                VStack {
+                    Text("\(self.timeVal)").font(.system(size: 40))
+                        .onAppear() {
+                        
+                            self.timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                                if self.timeVal > -1 {
+                                    self.timeVal -= 1
+                                }
+                            }
+                        }
+                    Button(action: {
+                        self.timer?.invalidate()
+                        self.timerScreenShow = false
+                        self.timeVal = 1
+                    }, label: {
+                        Text("Cancel")
+                            .foregroundColor(Color.red)
+                    })
+                    .padding(.top)
+                }
+            } else {
+                Button(action: {
+                    self.timer?.invalidate()
+                    self.timerScreenShow = false
+                    self.timeVal = 1
+                }, label: {
+                    Text("Done!")
+                        .font(.title)
+                        .foregroundColor(Color.green)
+                })/*.onAppear() {
+                    WKInterfaceDevice.current().play(.notification)
+                }*/
+            }
+        }.onDisappear() {
+            self.timer?.invalidate()
+            self.timeVal = 1
+        }
+    }
+}
+
+struct ContentView: View {
+    @State var timeVal = 1
+    @State var timerScreenShow:Bool = false
+    
+    var body: some View {
+        NavigationView{
+            VStack {
+                Text("Timer \(self.timeVal) seconds").font(.body)
+                Picker(selection: self.$timeVal, label: Text("")) {
+                    Text("1").tag(1).font(.title2)
+                    Text("5").tag(5).font(.title2)
+                    Text("10").tag(10).font(.title2)
+                    Text("30").tag(30).font(.title2)
+                    Text("60").tag(60).font(.title2)
+                }
+                NavigationLink(
+                    destination: TimerView(timerScreenShow: self.$timerScreenShow, timeVal: self.$timeVal),
+                    isActive: self.$timerScreenShow,
+                    label: {
+                        Text("Start")
+                    })
+            }
+        }
     }
 }
 
