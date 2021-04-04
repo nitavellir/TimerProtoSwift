541fd91009dc72c2f5fdd9e6c8540726c0be8d8d
rr <ryohamadapride@gmail.com>
2021-04-04 15:34:24 +0900

    minute/second picker

--- TimerProto/ContentView.swift
+++ TimerProto/ContentView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 import AudioToolbox
 
 struct TimerView: View {
-    @State var diff:Double = Double(0)
     @Binding var timerScreenShow:Bool
-    @Binding var timeVal:Double
-    
+    @State var timeVal:Double
+    @State var diff:Double = Double(0)
+
     @State var timer:Timer?
 
     var body: some View {
@@ -82,28 +82,56 @@ struct TimerView: View {
 }
 
 struct ContentView: View {
-    @State var timeVal:Double = 1
+    @State var minuteVal:Int = 0
+    @State var secondVal:Int = 1
     @State var timerScreenShow:Bool = false
 
     var body: some View {
         NavigationView{
+            GeometryReader { geometry in
             VStack {
-                Text("Timer \(Int(self.timeVal)) seconds").font(.body)
-                Picker(selection: self.$timeVal, label: Text("")) {
-                    Text("1").tag(Double(1)).font(.title2)
-                    Text("5").tag(Double(5)).font(.title2)
-                    Text("10").tag(Double(10)).font(.title2)
-                    Text("30").tag(Double(30)).font(.title2)
-                    Text("60").tag(Double(60)).font(.title2)
+                Text("Timer \(Int(self.minuteVal)):\(Int(self.secondVal))").font(.body)
+
+                HStack {
+                    Picker(selection: self.$minuteVal, label: Text("分")) {
+                        ForEach(0..<31) { num in
+                            Text("\(num)").tag(num).font(.title2)
+                        }
+                    }
+                    .frame(maxWidth: geometry.size.width / 4)
+                    .clipped()
+
+                    Text("分").frame(width: 50, height: 30)
+                        .frame(maxWidth: geometry.size.width / 4)
+                        .clipped()
+
+                    Picker(selection: self.$secondVal, label: Text("秒")) {
+                        ForEach(0..<61) { num in
+                            Text("\(num)").tag(num).font(.title2)
+                        }
+                    }
+                    .frame(maxWidth: geometry.size.width / 4)
+                    .clipped()
+
+                    Text("秒").frame(width: 50, height: 30)
+                        .frame(maxWidth: geometry.size.width / 4)
+                        .clipped()
                 }
+
                 NavigationLink(
-                    destination: TimerView(timerScreenShow: self.$timerScreenShow, timeVal: self.$timeVal),
+                    destination: TimerView(timerScreenShow: self.$timerScreenShow, timeVal: getSecond(minute: self.minuteVal, second: self.secondVal)),
                     isActive: self.$timerScreenShow,
                     label: {
                         Text("Start")
                     })
+                    .padding()
+            }
             }
         }
+    }
+
+    private func getSecond(minute:Int, second:Int) -> Double {
+        return Double(Double(minute) * Double(60) + Double(second))
     }
 }
 
